8b69e66838bf12d3a7181e401dcb9f0ff62ce01e
Brian Samuels <samuels.brian9@gmail.com>
2022-05-12 19:56:57 -0400

    creating all base folders and files and starter code

---
+++ models/Thoughts.js
@@ -0,0 +1,30 @@
+const {Schema, model}= require('mongoose');
+const formatDate = require('../utils/helper');
+const reactionSchema = require('.reaction');
+
+const thoughtSchema = new Schema (
+    {
+        throughText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            get: date => formatDate(date)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    }
+)
+
+thoughtSchema.virtual('reactionCount').get(()=> this.reactions.length);
+
+const Thoughts = model('Thoughts', thoughtSchema);
+
+module.exports = Thoughts;
---
+++ models/Users.js
@@ -0,0 +1,41 @@
+const {Schema, model} = require('mongoose');
+
+const userSchema = new Schema(
+    { 
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please fill a valid email address']
+        },
+
+        thoughts: [
+            {type: Schema.Types.ObjectId,
+            ref: 'Thoughts'}
+        ],
+
+        friends: [
+            {type: Schema.Types.ObjectId,
+            ref: 'Users'}
+        ]
+    }, 
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+userSchema.virtual('friendCount').get(()=> this.friends.length);
+
+const Users = model('Users', userSchema);
+
+module.exports = Users;
